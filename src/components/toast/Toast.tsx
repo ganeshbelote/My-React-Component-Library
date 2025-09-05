b6632b4b7bb5692c8108d958bd3eea6019d12259
ganeshbelote <ganeshbelote18@gmail.com>
2025-09-05 19:27:56 +0530

    Added : star svg to toast component

--- src/components/toast/Toast.tsx
+++ src/components/toast/Toast.tsx
@@ -153,6 +153,15 @@ export const Toast = ({
                 {/* Content */}
                 <div>
                   <h4 className='flex gap-2'>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      className='w-5 h-5 blink'
+                      viewBox='0 0 24 24'
+                      fill='currentColor'
+                    >
+                      <path d='M12 2l1.7 5.3 5.3 1.7-5.3 1.7-1.7 5.3-1.7-5.3-5.3-1.7 5.3-1.7L12 2z' />
+                      <path d='M4 12l1 .3.3 1L4 13l-1-.3-.3-1L4 12zm16 0l1 .3.3 1L20 13l-1-.3-.3-1L20 12zM12 20l.3 1 1 .3-1 .3-.3 1-.3-1-1-.3 1-.3.3-1z' />
+                    </svg>
                     <span className='font-medium text-sm lg:text-[16px]'>
                       {capitalize(toast.type)} notification
                     </span>
